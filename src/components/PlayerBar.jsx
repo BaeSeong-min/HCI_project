1e01e677eb50329a423d939c03a1e92211ba1dcb
seongmin <sm4046@naver.com>
2025-11-20 21:52:58 +0900

    feat/음악 진행 바 구현 및 선택한 음악의 시점으로 이동하는 기능 구현

--- src/components/PlayerBar.jsx
+++ src/components/PlayerBar.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { FiPlay, FiPause, FiSkipBack, FiSkipForward } from "react-icons/fi";
+import "../PlayerBar.css"
 
 function PlayerBar({ audioRef }) {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [progress, setProgress] = useState(0);
 
   const handlePlayClick = () => {
     if (!audioRef.current?.src) return;
@@ -16,14 +18,41 @@ function PlayerBar({ audioRef }) {
     }
   };
 
-  const progress = 40;
+  // 음악 진행바 업데이트 함수
+  const updateProgress = () => {
+    const current = audioRef.current.currentTime;
+    const total = audioRef.current.duration;
+
+    if (total > 0) {
+      setProgress((current / total) * 100);
+    }
+  }
+
+  // 선택한 특점 시점으로 음악 이동
+  const handleSeek = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const clickX = e.clientX - rect.left;
+    const width = rect.width;
+
+    const total = audioRef.current.duration;
+    const newTime = (clickX / width) * total;
+
+    audioRef.current.currentTime = newTime;
+  };
 
   return (
     <div className="player-container">
-      <audio ref={audioRef} />
+      <audio 
+        ref={audioRef} 
+        onTimeUpdate={updateProgress}
+        onEnded={() => setIsPlaying(false)}
+        />
 
-      <div className="progress-container">
-        <div className="progress-bar" style={{ width: `${progress}%` }}/>
+      <div className="progress-container" onClick={handleSeek}>
+        <div 
+          className="progress-bar" 
+          style={{ width: `${progress}%` }}
+        />
       </div>
 
       <div className="player-controls">
