e3617966a369abf5e932667f1da2d3aa0355accf
pyoseonchoi <eodhkdtlsrl@naver.com>
2025-11-22 16:20:01 +0900

    UI 일부 수정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -36,7 +36,7 @@ function MainPage({ playlist, currentSong, addSong, playNext, playPrev, playSpec
       <div style={{ display: 'flex', width: '100%', height: '60vh', alignItems: 'flex-start', marginTop: '20px' }}>
         
         {/* 왼쪽 메뉴 */}
-        <div className="left-menu" style={{ width: '20%', display: 'flex', flexDirection: 'column', gap: '15px' }}>
+        <div className="left-menu" style={{ width: '10%', display: 'flex', flexDirection: 'column', gap: '15px' }}>
           <button className={`sound-btn ${location.pathname === "/" ? "active" : ""}`} onClick={() => navigate("/")}>none</button>
           <button className={`sound-btn ${location.pathname === "/RainPage" ? "active" : ""}`} onClick={() => navigate("/RainPage")}>rain</button>
           <button className={`sound-btn ${location.pathname === "/BirdPage" ? "active" : ""}`} onClick={() => navigate("/BirdPage")}>bird</button>
@@ -46,14 +46,14 @@ function MainPage({ playlist, currentSong, addSong, playNext, playPrev, playSpec
         <div className="center-area" style={{ flex: 1, textAlign: 'center', padding: '0 20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%' }}>
            {currentSong && (
              <div style={{ color: '#333' }}>
-               <h2 style={{ fontSize: '2rem', marginBottom: '10px' }}>Now Playing</h2>
+              <h2 style={{ fontSize: '2rem', marginBottom: '100px' }}></h2>
                <p style={{ fontSize: '1.2rem' }}>{currentSong.name}</p>
              </div>
            )}
         </div>
 
         {/* 오른쪽 플레이리스트 */}
-        <div className="right-playlist" style={{ width: '25%', background: 'rgba(255,255,255,0.2)', backdropFilter: 'blur(5px)', padding: '20px', borderRadius: '15px', height: '100%', overflowY: 'auto' }}>
+        <div className="right-playlist" style={{ width: '20%', background: 'rgba(255,255,255,0.2)', backdropFilter: 'blur(5px)', padding: '20px', borderRadius: '15px', height: '100%', overflowY: 'auto' }}>
           <h3 style={{ borderBottom: '2px solid #fff', paddingBottom: '10px', color: '#333' }}>Playlist ({playlist.length})</h3>
           <ul style={{ listStyle: 'none', padding: 0, textAlign: 'left' }}>
             {playlist.map((file, index) => (
